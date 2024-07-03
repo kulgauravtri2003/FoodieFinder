bd9d89c2a62b0b22f7f10fd46edb689e2e4868e7
Kulgaurav_Tripathi <149904159+kulgauravtri2003@users.noreply.github.com>
2024-07-03 20:30:08 +0530

    Update StoreContext.jsx

--- Fronted/src/context/StoreContext.jsx
+++ Fronted/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@
 // const StoreContextProvider = (props) => {
 
 // const [cartItems,setCartItems]=useState({});
-// const url="http://localhost:4000"
+// const url=https://foodiefinder-0eht.onrender.com
 // const [token,setToken]=useState("");
 // const [Food_list,setFoodList]=useState([])
 
