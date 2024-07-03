6b122bca6a102b353f59ce3d48b5d47b90b1ece1
Kulgaurav_Tripathi <149904159+kulgauravtri2003@users.noreply.github.com>
2024-07-03 20:31:07 +0530

    Update StoreContext.jsx

--- Fronted/src/context/StoreContext.jsx
+++ Fronted/src/context/StoreContext.jsx
@@ -84,7 +84,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = https://foodiefinder-0eht.onrender.com
     const [token,setToken] = useState("")
     const [food_list,setFoodList] = useState([])
 
